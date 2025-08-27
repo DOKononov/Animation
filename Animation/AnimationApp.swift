7cdec06de97661c03674518f9cb6a75094f53a60
d.o.kononov <d.o.kononov@gmail.com>
2025-08-27 15:52:35 +0300

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Dmitry Kononov on 27.08.25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
