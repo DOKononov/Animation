b622e3028a081673722774400c4470e34f3d5afd
d.o.kononov <d.o.kononov@gmail.com>
2025-08-27 17:02:44 +0300

    transition animation

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -7,30 +7,48 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    var amount: Double
+    var anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(CGFloat(amount)), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: .zero, anchor: .topLeading)
+        )
+    }
+    
+}
+
 struct ContentView: View {
-    private let letters = Array("Hello SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+    @State private var isShowingRed = false
     
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
-        )
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
     }
 }
 
